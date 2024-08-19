ab5808e1f260800c0f7a124dc324d2dbdac4f295
Ange Bagui <ange.bagui@donypay.com>
2024-08-19 20:12:57 +0000

    TypeEnginModel updated

--- app/Http/Controllers/API/OrderAPIController.php
+++ app/Http/Controllers/API/OrderAPIController.php
@@ -1185,7 +1185,7 @@ class OrderAPIController extends AppBaseController
 
     $inner_radius = 0;
 
-    $outer_radius = 20;
+    $outer_radius = 30;
 
     $destination_point = $destination_list->last();
 
