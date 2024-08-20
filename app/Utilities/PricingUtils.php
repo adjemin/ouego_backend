17231e2c86bd36676cecfe4d209287400e5376a6
Ange Bagui <ange.bagui@donypay.com>
2024-08-20 08:46:01 +0000

    updated

--- app/Utilities/PricingUtils.php
+++ app/Utilities/PricingUtils.php
@@ -92,7 +92,8 @@ class PricingUtils{
 
         $prix_base = doubleval($typeEnginModel->ride_base_pricing);
 
-        $frais_route = doubleval(Setting::get('FRAIS_ROUTE'));
+        $frais_route = 0;
+        //$frais_route = doubleval(Setting::get('FRAIS_ROUTE'));
 
         $initial_distance = $distance;
 
