00ed77ed253c761d781440abf41fa799cabfde72
Ange Bagui <ange.bagui@donypay.com>
2024-09-22 16:32:30 +0000

    .env updated

--- config/database.php
+++ config/database.php
@@ -74,7 +74,7 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            'search_path' => 'public',
+            'search_path' => 'ouego_database',
             'sslmode' => 'prefer',
             'schema' => env('DB_SCHEMA', 'ouego_database'),
         ],
