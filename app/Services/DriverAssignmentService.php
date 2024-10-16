f4043b2df0a511c2c3eef5ef5f258209e98d99f0
Ange Bagui <ange.bagui@donypay.com>
2024-10-16 07:52:31 +0000

    updated

--- app/Services/DriverAssignmentService.php
+++ app/Services/DriverAssignmentService.php
@@ -31,7 +31,7 @@ class DriverAssignmentService
         ->selectRaw('ST_Distance(last_location::geography, ST_SetSRID(ST_MakePoint(?, ?), 4326)::geography) as distance', [$longitude, $latitude])
         ->whereRaw('is_available = true')
         ->whereRaw('is_active = true')
-        ->whereRaw('updated_at >= NOW() - INTERVAL ? MINUTE', [$maxUpdateTime])
+        ->whereRaw("updated_at >= NOW() - INTERVAL '{$maxUpdateTime} MINUTE'")
         ->whereJsonContains('services', $service_slug)
         ->orderByRaw('last_location <-> ST_SetSRID(ST_MakePoint(?, ?), 4326)::geography', [$longitude, $latitude]);
 
