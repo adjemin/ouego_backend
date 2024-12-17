7c698e9f3a647f9e9cdeecb23f45595196d64225
Ange Bagui <ange.bagui@donypay.com>
2024-12-17 23:26:43 +0000

    updated

--- app/Jobs/SendTestPushNotificationJob.php
+++ app/Jobs/SendTestPushNotificationJob.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
+use App\Models\DriverDevice;
 
 class SendTestPushNotificationJob implements ShouldQueue
 {
