66dc2e9f5270bc640d1d75ad97d8f9784eee3e2f
Ange Bagui <ange.bagui@donypay.com>
2024-12-17 12:01:54 +0000

    updated

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
