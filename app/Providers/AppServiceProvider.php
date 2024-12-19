24e9b180bcb343f20b87622f27e575d529053052
Ange Bagui <ange.bagui@donypay.com>
2024-12-19 20:36:53 +0000

    updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Queue\Events\JobFailed;
+use Illuminate\Support\Facades\Queue;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +22,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Queue::failing(function (JobFailed $event) {
+            // Notification ou logging personnalisé
+            \Log::error('Job failed', [
+                'job' => get_class($event->job),
+                'exception' => $event->exception->getMessage(),
+            ]);
+        });
     }
 }
