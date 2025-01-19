1d4f7658dcc8ac087f207680af54b6aace999e1a
Ange Bagui <ange.bagui@donypay.com>
2025-01-19 15:45:54 +0000

    updated

--- app/Jobs/SendPushNotification.php
+++ app/Jobs/SendPushNotification.php
@@ -65,7 +65,7 @@ class SendPushNotification implements ShouldQueue
             $notification = [
                 'title' => $this->notificationData->title,
                 'body' => $this->notificationData->subtitle,
-                'sound' => 'default',
+                'sound' => 'notification_sound.wav',
                 'badge' => '1',
                 'type' => $this->notificationData->type,
                 'id' => $this->notificationData->id
