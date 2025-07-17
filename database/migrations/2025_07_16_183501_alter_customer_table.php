767cf46b86ea33d79590fd5f8a0edef0b7427c9e
John <lamagniguitoure@gmail.com>
2025-07-17 13:36:17 +0000

    update migrations

--- database/migrations/2025_07_16_183501_alter_customer_table.php
+++ database/migrations/2025_07_16_183501_alter_customer_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('customer_address', function (Blueprint $table) {
-            //
+            $table->dropColumn('customer_id');
         });
     }
 };
