a8fb2821dbb837b664815452f494394fbc483f17
John <lamagniguitoure@gmail.com>
2025-09-26 06:47:29 +0000

    fix: attribute orders

--- app/Http/Controllers/API/OrderAPIController.php
+++ app/Http/Controllers/API/OrderAPIController.php
@@ -1490,14 +1490,14 @@ class OrderAPIController extends AppBaseController
         // Register order history
         $order->newOrderHistory(Order::PERFORMER_LOOKUP, $customer->table, $customer->id);
 
-         $route_point = RoutePoint::where([
-           'order_id' => $order->id,
-           'type' => 'source'
-        ])->first();
+        //  $route_point = RoutePoint::where([
+        //    'order_id' => $order->id,
+        //    'type' => 'source'
+        // ])->first();
 
-        $this->driverAssignmentService->findNearestDrivers($order->service_slug, $route_point->latitude, $route_point->longitude, 5);
+        // $this->driverAssignmentService->findNearestDrivers($order->service_slug, $route_point->latitude, $route_point->longitude, 5);
 
-        // $this->driverAssignmentService->assignNearestDriver($order);
+        $this->driverAssignmentService->assignNearestDriver($order);
 
         return $this->sendResponse($order->toArray(), 'Order updated successfully');
 
